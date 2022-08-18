07c5d50850d290c5b40f0bff40fc5705a30a399c
muhammad sami <muhammadsami290@outlook.com>
2022-08-18 21:23:06 +0500

    test_home_page refactor

--- server/apps/main/views.py
+++ server/apps/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from server.apps.hotels.models import Hotel
 
 
 def index(request):
     return render(request, 'index.html', status=200)
+
+
+def get_hotel_data():
+    return len(Hotel.objects.all())
