608d463d96393be7461fad3e8e575c874ba966d7
muhammad sami <muhammadsami290@outlook.com>
2022-08-20 12:03:31 +0500

    Template context hotel and tour test to green

--- server/apps/main/tests.py
+++ server/apps/main/tests.py
@@ -1,9 +1,26 @@
-from urllib import response
-from django.test import Client, TransactionTestCase
+from django.test import Client, TestCase
 from server.apps.hotels.models import Hotel
+from server.apps.tours.models import Tour
 
 
-class HomePageTest(TransactionTestCase):
+def create_test_hotel_object(hotel_name: str, no_of_rooms: int, price_per_night: int, hotel_description: str) -> Hotel:
+    return Hotel.objects.create(hotel_name=hotel_name,
+                                no_of_rooms=no_of_rooms,
+                                price_per_night=price_per_night,
+                                hotel_description=hotel_description)
+
+
+def create_test_tour_object(tour_name: str, tour_price: int,
+                            tour_description: str, max_participants: int,
+                            tour_duration: int) -> Tour:
+    return Tour.objects.create(tour_name=tour_name,
+                               tour_price=tour_price,
+                               tour_description=tour_description,
+                               max_participants=max_participants,
+                               tour_duration=tour_duration)
+
+
+class HomePageTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.response = self.client.get('/')
@@ -13,5 +30,7 @@ class HomePageTest(TransactionTestCase):
         self.assertTemplateUsed(self.response, 'index.html')
 
     def test_template_contains_correct_context(self):
-        self.assertCountEqual(
-            self.response.context['hotels'], Hotel.objects.all()[:4])
+        self.assertQuerysetEqual(
+            self.response.context['hotels'], [create_test_hotel_object('Transalvania', 100, 200, 'Best monster hotel')])
+        self.assertQuerysetEqual(self.response.context['tours'],
+                                 [create_test_tour_object('Mountain View', 150, 'Best tour', 20, 15)])
--- server/apps/main/views.py
+++ server/apps/main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from server.apps.hotels.models import Hotel
+from server.apps.tours.models import Tour
 
 
 def index(request):
-    hotels = Hotel.objects.all()
-    context = {'hotels': hotels}
+    hotels = Hotel.objects.all()[:4]
+    tours = Tour.objects.all()[:4]
+    context = {
+        'hotels': hotels,
+        'tours': tours,
+
+    }
     return render(request, 'index.html', status=200, context=context)
