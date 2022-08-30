173b5985e60030eea17439a753629e5111d142b1
Muhammad Sami <muhammadsami290@outlook.com>
2022-08-30 18:47:50 +0500

    Tour model context seggregated into single attribute

--- server/apps/tours/tests.py
+++ server/apps/tours/tests.py
@@ -2,22 +2,37 @@ from django.test import TestCase, Client
 from .utils.context_objects import *
 
 
+def create_test_tour_object():
+    return Tour.objects.create(tour_name='test_tour', tour_price=0, tour_description='', max_participants=0, tour_duration=0)
+
+
 class TourPageTest(TestCase):
     def setUp(self):
         super().setUp()
         self.client = Client()
-        self.response = self.client.get('/tours/<str:tour>')
+        self.tour = create_test_tour_object()
+        self.response = self.client.get('/tours/test_tour')
 
     def test_tour_page_returns_correct_template(self):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'tour.html')
 
     def test_tour_page_contains_correct_context(self):
+        self.tour_name = 'test_tour'
+
         self.assertEqual(
-            self.response.context['tour'], get_tour('test_tour'))
+            self.response.context['tour_name'], get_tour_name(self.tour_name))
         self.assertEqual(
-            self.response.context['tour_rating'], get_tour_rating(self.response.context['tour']))
-        self.assertEqual(self.response.context['tour_images'], get_tour_images(
-            self.response.context['tour']))
-        self.assertEqual(self.response.context['tour_instructor'], get_tour_instructor(
-            self.response.context['tour']))
+            self.response.context['tour_price'], get_tour_price(self.tour_name))
+        self.assertEqual(
+            self.response.context['tour_description'], get_tour_description(self.tour_name))
+        self.assertEqual(
+            self.response.context['max_participants'], get_tour_max_participants(self.tour_name))
+        self.assertEqual(
+            self.response.context['tour_duration'], get_tour_duration(self.tour_name))
+        # self.assertEqual(
+        #     self.response.context['tour_rating'], get_tour_rating(self.response.context['tour']))
+        # self.assertEqual(self.response.context['tour_images'], get_tour_images(
+        #     self.response.context['tour']))
+        # self.assertEqual(self.response.context['tour_instructor'], get_tour_instructor(
+        #     self.response.context['tour']))
--- server/apps/tours/utils/context_objects.py
+++ server/apps/tours/utils/context_objects.py
@@ -7,30 +7,58 @@ from server.apps.tours.models import Tour
 
 
 def get_context(tour_name: str) -> dict:
-    tour = get_tour(tour_name)
-    tour_rating = get_tour_rating(tour)
-    tour_images = get_tour_images(tour)
-    tour_instructor = get_tour_instructor(tour)
+    # tour = get_tour(tour_name)
+    # tour_rating = get_tour_rating(tour)
+    # tour_images = get_tour_images(tour)
+    # tour_instructor = get_tour_instructor(tour)
+    tour_name = get_tour_name(tour_name)
+    tour_price = get_tour_price(tour_name)
+    tour_description = get_tour_description(tour_name)
+    max_participants = get_tour_max_participants(tour_name)
+    tour_duration = get_tour_duration(tour_name)
 
     return {
-        'tour': tour,
-        'tour_rating': tour_rating,
-        'tour_images': tour_images,
-        'tour_instructor': tour_instructor,
+        'tour_name': tour_name,
+        'tour_price': tour_price,
+        'tour_description': tour_description,
+        'max_participants': max_participants,
+        'tour_duration': tour_duration,
+        # 'tour_rating': tour_rating,
+        # 'tour_images': tour_images,
+        # 'tour_instructor': tour_instructor,
     }
 
 
-def get_tour(tour_name: str) -> list:
-    return [tour for tour in Tour.objects.filter(tour_name=tour_name)]
+# def get_tour(tour_name: str) -> list:
+#     return [tour for tour in Tour.objects.filter(tour_name=tour_name)]
 
 
-def get_tour_rating(tour: Tour) -> list:
-    return [Rating.objects.filter(pk=tour.rating.pk)[0] for tour in TourRating.objects.filter(tour__in=tour)[:1]]
+def get_tour_name(tour_name: str) -> list:
+    return Tour.objects.filter(tour_name=tour_name)[0].tour_name
 
 
-def get_tour_images(tour: Tour) -> list:
-    return [tour_image for tour_image in TourImage.objects.filter(tour__in=tour)[:4]]
+def get_tour_price(tour_name: str) -> list:
+    return Tour.objects.filter(tour_name=tour_name)[0].tour_price
 
 
-def get_tour_instructor(tour: Tour) -> list:
-    return [Instructor.objects.filter(pk=tour_instructor.instructor.pk)[0] for tour_instructor in TourInstructor.objects.filter(tour__in=tour)]
+def get_tour_duration(tour_name: str) -> list:
+    return Tour.objects.filter(tour_name=tour_name)[0].tour_duration
+
+
+def get_tour_description(tour_name: str) -> list:
+    return Tour.objects.filter(tour_name=tour_name)[0].tour_description
+
+
+def get_tour_max_participants(tour_name: str) -> list:
+    return Tour.objects.filter(tour_name=tour_name)[0].max_participants
+
+# def get_tour_rating(tour: Tour) -> list:
+#     return [Rating.objects.filter(pk=tour.rating.pk)[0] for tour in TourRating.objects.filter(tour__in=tour)[:1]]
+
+
+# def get_tour_images(tour: Tour) -> list:
+#     return [tour_image for tour_image in TourImage.objects.filter(tour__in=tour)[:4]]
+
+
+# def get_tour_instructor(tour: Tour) -> list:
+#     return [Instructor.objects.filter(pk=tour_instructor.instructor.pk)[0] for tour_instructor in TourInstructor.objects.filter(tour__in=tour)]
