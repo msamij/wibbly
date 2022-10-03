6701ad3ca26689d4395a41d98e0bdd9892ee305c
Muhammad Sami <muhammadsami290@outlook.com>
2022-10-03 07:38:06 +0500

    m

--- client/ui/event functions/logout.ts
+++ client/ui/event functions/logout.ts
@@ -3,7 +3,6 @@ import { HTTP } from '@networking/http';
 import { Urls } from '@networking/urls';
 
 export async function logout() {
-  const response = await HTTP.post(`${Urls.baseUrl}${Urls.authUrl}${Urls.logoutUrl}`);
-  console.log(response.status);
+  await HTTP.post(`${Urls.baseUrl}${Urls.authUrl}${Urls.logoutUrl}`);
   toggleLogoutButton(false);
 }
